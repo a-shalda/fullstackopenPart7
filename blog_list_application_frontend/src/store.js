a501fb08e365c8557d9d553f7829750c4d552831
Alex Shalda <123298731+a-shalda@users.noreply.github.com>
2023-12-09 11:56:36 +0400

    fixed the store

--- blog_list_application_frontend/src/store.js
+++ blog_list_application_frontend/src/store.js
@@ -8,7 +8,8 @@ const store = configureStore({
   reducer: {
     notification: notificationReducer,
     blogs: blogReducer,
-    user: userReducer
+    user: userReducer,
+    users: allUsersReducer
   }
 })
 
